ca8023ea5f2b7ebc7f387167a8d249722666c26c
Andrew Williamson <awilliamson@Jerue.com>
2018-03-26 16:29:21 -0400

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,15 @@
+/* scripts.js */ 
+//Andrew Williamson
+
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        'anchors': ['page1', 'page2', 'page3', 'page4', 'page5'],
+        'lockanchors': true,
+        'verticalCentered': true,
+        'horizontalCentered': true,
+        'showActiveTooltip': true,
+        'css3': true,
+        'sectionsColor': ['#102444', '#102444', '#0A4138', '#8f3985', '#a675a1']
+    });
+  });
+
