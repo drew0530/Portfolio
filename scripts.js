5fb4765930035d12131c0e262c32706d08d12681
Andrew Williamson <awilliamson@Jerue.com>
2018-03-28 13:25:34 -0400

    Installed packages and added some parallax effect Text

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 /* scripts.js */ 
 //Andrew Williamson
 
+
 $(document).ready(function() {
     $('#fullpage').fullpage({
         'anchors': ['page1', 'page2', 'page3', 'page4', 'page5'],
@@ -11,5 +12,7 @@ $(document).ready(function() {
         'css3': true,
         'sectionsColor': ['#102444', '#102444', '#0A4138', '#8f3985', '#a675a1']
     });
-  });
+    var scene = document.getElementById('scene');
+    var parallaxInstance = new Parallax(scene);
+});
 
