22b98d2f953f51ba4f3be7e979b2ffb4f68ac922
Andrew Williamson <awilliamson@Jerue.com>
2018-03-28 13:37:29 -0400

    Revert "pushed packages to master"
    This reverts commit 8788722f10797281609cf415e4363aceeef0f8da.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
 /* scripts.js */ 
 //Andrew Williamson
 
-
 $(document).ready(function() {
     $('#fullpage').fullpage({
         'anchors': ['page1', 'page2', 'page3', 'page4', 'page5'],
@@ -12,7 +11,5 @@ $(document).ready(function() {
         'css3': true,
         'sectionsColor': ['#102444', '#102444', '#0A4138', '#8f3985', '#a675a1']
     });
-    var scene = document.getElementById('scene');
-    var parallaxInstance = new Parallax(scene);
-});
+  });
 
