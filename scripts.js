1a8493464ef8f404331706b07b991062195f93b2
Drew Williamson <drewman0530@yahoo.com>
2018-03-29 13:13:29 -0400

    removed animation because of import issue

--- scripts.js
+++ scripts.js
@@ -1,6 +1,5 @@
 /* scripts.js */ 
 //Andrew Williamson
-import anime from 'anime.js';
 
 $(document).ready(function() {
     // Load FullPage Instance
@@ -18,15 +17,5 @@ $(document).ready(function() {
     // Load Parallax Instance
     var scene = document.getElementById('scene');
     var parallaxInstance = new Parallax(scene);
-    // Load Animations
-    var lineDrawing = anime({
-        targets: '#lineDrawing .lines path',
-        strokeDashoffset: [anime.setDashoffset, 0],
-        easing: 'easeInOutSine',
-        duration: 1500,
-        delay: function (el, i) { return i * 250 },
-        direction: 'alternate',
-        loop: true
-    });
 });
 
