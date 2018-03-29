3b74a6009d767f93d51f33f65abb71373fa345f8
Drew Williamson <drewman0530@yahoo.com>
2018-03-29 13:09:46 -0400

    Added some layout changes and anime.js module

--- scripts.js
+++ scripts.js
@@ -1,8 +1,9 @@
 /* scripts.js */ 
 //Andrew Williamson
-
+import anime from 'anime.js';
 
 $(document).ready(function() {
+    // Load FullPage Instance
     $('#fullpage').fullpage({
         'anchors': ['page1', 'page2', 'page3', 'page4', 'page5'],
         'lockanchors': true,
@@ -10,9 +11,22 @@ $(document).ready(function() {
         'horizontalCentered': true,
         'showActiveTooltip': true,
         'css3': true,
-        'sectionsColor': ['#102444', '#102444', '#102444', '#102444', '#102444']
+        'sectionsColor': ['#102444', '#102444', '#102444', '#102444', '#102444'],
+        // 'navigation': true,
+        // 'navigationPosition': 'left'
     });
+    // Load Parallax Instance
     var scene = document.getElementById('scene');
     var parallaxInstance = new Parallax(scene);
+    // Load Animations
+    var lineDrawing = anime({
+        targets: '#lineDrawing .lines path',
+        strokeDashoffset: [anime.setDashoffset, 0],
+        easing: 'easeInOutSine',
+        duration: 1500,
+        delay: function (el, i) { return i * 250 },
+        direction: 'alternate',
+        loop: true
+    });
 });
 
