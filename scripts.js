e970631493f2b54390e9a46ac2d5be0c58ce97e4
Andrew Williamson <awilliamson@Jerue.com>
2018-04-20 17:00:17 -0400

    added navbar, still working on further frameworking

--- scripts.js
+++ scripts.js
@@ -2,6 +2,22 @@
 
 $(document).ready(function() {
     // Load FullPage Instance
+
+    //USE ME TO ENCAPSULATE ALL ANIMATION STUFF!!
+    // var buttonEl = document.querySelector('#menu');
+
+    // function animateButton(scale, duration, elasticity) {
+    //   anime.remove(buttonEl);
+    //   anime({
+    //     targets: buttonEl,
+    //     scale: scale,
+    //     duration: duration,
+    //     elasticity: elasticity
+    //   });
+    // }
+    // function enterButton() { animateButton(1.2, 800, 400) };
+    // function leaveButton() { animateButton(1.0, 600, 300) };
+    
     $('#fullpage').fullpage({
         'anchors': ['page1', 'page2', 'page3', 'page4', 'page5'],
         'verticalCentered': true,
@@ -35,6 +51,21 @@ $(document).ready(function() {
                 duration: 800,
                 delay: 250
             });
+            var navbar = anime({
+                targets: '#navbar',
+                translateY: [-50, 0],
+                opacity: {
+                   value: [0,1],
+                   duration: 800
+                },
+                duration:500,
+                delay: 700,
+                easing: 'easeInOutCubic'
+            })
+
+            // THEN YOU CAN JUST CALL ME LIKE THIS!!!!
+            // buttonEl.addEventListener('mouseenter', enterButton, false);
+            // buttonEl.addEventListener('mouseleave', leaveButton, false);
         },
         onLeave: function(index, nextIndex, direction){
             var titleText = anime.timeline({opacity:0});
@@ -58,6 +89,7 @@ $(document).ready(function() {
             });
         }
     });
+    
     // Load Parallax Instance
     var scene = document.getElementById('scene');
     var parallaxInstance = new Parallax(scene);
